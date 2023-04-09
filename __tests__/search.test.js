fc08cecf1277285f86246007b025444b2a9b8836
Konstantin Kuzin <wearebeard@gmail.com>
2023-04-09 17:29:09 +0300

    feat: improved tests

--- __tests__/search.test.js
+++ __tests__/search.test.js
@@ -2,6 +2,14 @@ import { describe, it, expect } from '@jest/globals';
 import search from '../src/index.js';
 
 describe('positive cases', () => {
+  it('border cases', () => {
+    const result1 = search([], 'hello');
+    const result2 = search([{ id: 'doc1', text: 'hello' }], '');
+
+    expect(result1).toEqual([]);
+    expect(result2).toEqual([]);
+  });
+
   it('search a word in docs', () => {
     const doc1 = { id: 'doc1', text: 'hello world' };
     const doc2 = { id: 'doc2', text: 'Im a simple text.' };
@@ -14,14 +22,14 @@ describe('positive cases', () => {
 });
 
 describe('negative cases', () => {
-  const badDocs = [[{}], [1], [''], [true]];
+  const badDocs = [[{}], [1], [''], [true], [null], [undefined]];
   it.each(badDocs)('bad docs %#', (docs) => {
     expect(() => {
       search(docs, 'hello');
     }).toThrow();
   });
 
-  const badPatterns = [[{}], [1], [[]], [true]];
+  const badPatterns = [[{}], [1], [[]], [true], [null], [undefined]];
   it.each(badPatterns)('bad pattern %#', (pattern) => {
     expect(() => {
       search([], pattern);
